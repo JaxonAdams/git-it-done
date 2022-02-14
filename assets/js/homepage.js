ec13c4f85c6706ec591bc4055243ddd84c74afae
Jaxon Adams <jadamsresume1@gmail.com>
2022-02-14 11:32:36 -0700

    reformatted fetch request to use variable inputs

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,13 @@
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-    });
-});
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-console.log("outside");
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+getUserRepos("JaxonAdams");
