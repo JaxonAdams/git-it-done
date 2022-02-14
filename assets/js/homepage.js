7a4ebc2588a2b48788f97e19a60ea8ac1d8dd439
Jaxon Adams <jadamsresume1@gmail.com>
2022-02-13 20:15:35 -0700

    created first fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
-var getUserRepos = function() {
-    console.log("Function was called.");
-};
+fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+    });
+});
 
-getUserRepos();
+console.log("outside");
